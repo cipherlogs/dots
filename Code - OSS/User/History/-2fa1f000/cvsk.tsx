1b19b77c06b83c86350869b2cf81af6178f9d018
cipherlogs <73669345+0x78logs@users.noreply.github.com>
2026-01-30 00:00:24 +0100

    remove code-oss tracking

--- Code - OSS/User/History/-2fa1f000/cvsk.tsx
+++
@@ -1,9 +0,0 @@
-import { MinimalGoalCard } from "@/app/components/stats/minimalGoalCard";
-
-export default function Page() {
-  return (
-    <Center>
-      <MinimalGoalCard />
-    </Center>
-  );
-}
